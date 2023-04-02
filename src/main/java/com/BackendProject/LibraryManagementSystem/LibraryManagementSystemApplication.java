76008b8bead8ee6954395ea4aca8b56a2be4a00e
Yuvraj <yv96875@gmail.com>
2023-04-02 22:11:07 +0530

    1

---
+++ src/main/java/com/BackendProject/LibraryManagementSystem/LibraryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.BackendProject.LibraryManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementSystemApplication.class, args);
+	}
+// http://localhost:8080/swagger-ui/index.html
+}
